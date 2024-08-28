c007afcda2418b3ade2b830392a12e5c29349a8b
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-08-28 15:06:14 +0100

    Remove commented out code

--- main.py
+++ main.py
@@ -10,55 +10,6 @@ chatbot = ChatBot()
 if 'responses' not in st.session_state:
     st.session_state.responses = []
 
-# # select model and training parameters
-# selected_model = chatbot.models[0]
-# temperature = 1.5
-
-# # define url of backend chat API
-# backend_url = "http://127.0.0.1:5000/chat_batch"
-
-# # handle sending requests and receiving responses
-# def handle_message(user_input):
-#     if user_input:
-#         # add user input to session state
-#         st.session_state.responses.append({'user': user_input, 'bot': None})
-
-#         # prepare empty container to update the bot's response in real time
-#         response_container = st.empty()
-
-#         # send the user input to backend
-#         response = requests.post(
-#             backend_url, 
-#             json={
-#                 "message": user_input,
-#                 "model": selected_model,
-#                 "temperature": temperature,
-#             },
-#             stream=True
-#         )
-#         if response.status_code == 200:
-#             bot_response = ""
-
-#             # collect the batch response
-#             for chunk in response.iter_content(chunk_size=None, decode_unicode=True):   
-#                 bot_response += chunk
-
-#             # display bot response with adaptable height
-#             st.markdown("Sharwie:")
-#             st.markdown(
-#                 f"""
-#                 <div style="background-color:#262730; padding:10px; border-radius: 5px;">
-#                     <p style="font-family: Arial, sans-serif; font-color: #2f2f2f">{bot_response.strip()}</p>
-#                 </div>
-#                 """,
-#                 unsafe_allow_html=True,
-#             )
-#         else:
-#             response_container.markdown(
-#                 "<p style='color: red;'>Error: Unable to get response from server.</p>",
-#                 unsafe_allow_html=True,
-#             )
-
 def handle_message(
     user_input,
     backend_url,
@@ -173,16 +124,6 @@ def main():
                 set_tokens=set_tokens,
                 temperature=temperature,
             )
-
-    
-            
-# # input text box for user input
-# if 'current_input' not in st.session_state:
-#     st.session_state.current_input = ""
-# user_input = st.text_input("You:", st.session_state.current_input)
-
-# if st.button("Send"):
-#     handle_message(user_input)
 
 if __name__ =="__main__":
     main()
